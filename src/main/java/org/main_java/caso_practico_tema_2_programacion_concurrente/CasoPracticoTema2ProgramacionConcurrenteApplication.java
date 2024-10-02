86cffbc5b12522451e4f60c351d59e5e5930bd3b
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-10-02 11:59:54 +0200

    Arquitectura Springboot

---
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/CasoPracticoTema2ProgramacionConcurrenteApplication.java
@@ -0,0 +1,13 @@
+package org.main_java.caso_practico_tema_2_programacion_concurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPracticoTema2ProgramacionConcurrenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoPracticoTema2ProgramacionConcurrenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/main_java/caso_practico_tema_2_programacion_concurrente/CasoPracticoTema2ProgramacionConcurrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main_java.caso_practico_tema_2_programacion_concurrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoTema2ProgramacionConcurrenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
