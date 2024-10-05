d5ed749fe92d828ff6b8c5182ce2a0263741baf5
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-10-05 21:02:36 +0200

    repos
    Interfaces [JPA]

---
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/repos/SampleRepository.java
@@ -0,0 +1,10 @@
+package org.main_java.caso_practico_tema_2_programacion_concurrente.repos;
+
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Sample;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SampleRepository extends JpaRepository<Sample, Long> {
+}
+
