46e4a369a30d4b57f3a50e2c63fa644991fb8967
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-10-06 15:55:31 +0200

    SampleService
    SampleService y cambios en SampleResource para eliminar el uso de tokens por el momento.

--- src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/rest/SampleResource.java
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/rest/SampleResource.java
@@ -19,36 +19,44 @@ public class SampleResource {
         this.sampleService = sampleService;
     }
 
-    @PostMapping("/list")
+    // Retorna todas las muestras
+    @GetMapping("/list")
     @ApiResponse(responseCode = "200", description = "Get all samples")
-    public ResponseEntity<List<SampleDTO>> getAllSamples(@RequestHeader("Authorization") String token) {
-        return ResponseEntity.ok(sampleService.findAll(token));
+    public ResponseEntity<List<SampleDTO>> getAllSamples() {
+        return ResponseEntity.ok(sampleService.findAll());
     }
 
+    // Obtiene un Sample específico por ID
     @GetMapping("/{id}")
-    public ResponseEntity<SampleDTO> getSample(@PathVariable final Long id, @RequestHeader("Authorization") String token) {
+    @ApiResponse(responseCode = "200", description = "Get sample by ID")
+    public ResponseEntity<SampleDTO> getSample(@PathVariable final Long id) {
         SampleDTO sample = sampleService.get(id);
         return ResponseEntity.ok(sample);
     }
 
+    // Crea un nuevo Sample
     @PostMapping
     @ApiResponse(responseCode = "201", description = "Create a new sample")
-    public ResponseEntity<Long> createSample(@RequestBody final SampleDTO sampleDTO, @RequestHeader("Authorization") String token) {
+    public ResponseEntity<Long> createSample(@RequestBody final SampleDTO sampleDTO) {
         Long createdId = sampleService.create(sampleDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
+    // Actualiza un Sample existente
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateSample(@PathVariable final Long id, @RequestBody final SampleDTO sampleDTO, @RequestHeader("Authorization") String token) {
+    @ApiResponse(responseCode = "200", description = "Update an existing sample")
+    public ResponseEntity<Void> updateSample(@PathVariable final Long id, @RequestBody final SampleDTO sampleDTO) {
         sampleService.update(id, sampleDTO);
         return ResponseEntity.ok().build();
     }
 
+    // Elimina un Sample por su ID
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204", description = "Delete a sample")
-    public ResponseEntity<Void> deleteSample(@PathVariable final Long id, @RequestHeader("Authorization") String token) {
+    public ResponseEntity<Void> deleteSample(@PathVariable final Long id) {
         sampleService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
+
 
--- src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/service/SampleService.java
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/service/SampleService.java
@@ -1,4 +1,127 @@
 package org.main_java.caso_practico_tema_2_programacion_concurrente.service;
 
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.BiologicalData;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Experiment;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.model.BiologicalDataDTO;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.model.SampleDTO;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.repos.BiologicalDataRepository;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.repos.ExperimentRepository;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.repos.SampleRepository;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Sample;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.util.NotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class SampleService {
+
+    private final SampleRepository sampleRepository;
+    private final ExperimentRepository experimentRepository;
+    private final BiologicalDataRepository biologicalDataRepository;
+
+    public SampleService(SampleRepository sampleRepository, ExperimentRepository experimentRepository, BiologicalDataRepository biologicalDataRepository) {
+        this.sampleRepository = sampleRepository;
+        this.experimentRepository = experimentRepository;
+        this.biologicalDataRepository = biologicalDataRepository;
+    }
+
+
+    public List<SampleDTO> findAll() {
+        final List<Sample> samples = sampleRepository.findAll(Sort.by("id"));
+        return samples.stream()
+                .map(sample -> mapToDTO(sample, new SampleDTO()))
+                .collect(Collectors.toList());
+    }
+
+    public SampleDTO get(final Long id) {
+        return sampleRepository.findById(id)
+                .map(sample -> mapToDTO(sample, new SampleDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final SampleDTO sampleDTO) {
+        Sample sample = new Sample();
+        mapToEntity(sampleDTO, sample);
+        return sampleRepository.save(sample).getId();
+    }
+
+    public void update(final Long id, final SampleDTO sampleDTO) {
+        Sample sample = sampleRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(sampleDTO, sample);
+        sampleRepository.save(sample);
+    }
+
+    public void delete(final Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("El id no puede ser null");
+        }
+        sampleRepository.deleteById(id);
+    }
+
+    private SampleDTO mapToDTO(final Sample sample, final SampleDTO sampleDTO) {
+        // Asignamos el ID de la entidad Sample al DTO
+        sampleDTO.setId(sample.getId());
+
+        // Asignamos el código de la muestra [SampleCode] de la entidad Sample al DTO
+        sampleDTO.setSampleCode(sample.getSampleCode());
+
+        // Asignamos el ID del experimento relacionado [Experiment] al DTO
+        sampleDTO.setExperimentId(sample.getExperiment().getId());
+
+        // Creación de un nuevo DTO para BiologicalData y mapeo de sus propiedades
+        BiologicalDataDTO biologicalDataDTO = new BiologicalDataDTO();
+
+        // Asignamos el ID del BiologicalData relacionado al DTO
+        biologicalDataDTO.setId(sample.getBiologicalData().getId());
+
+        // Asignamos el tipo de muestra [SampleType] de BiologicalData al DTO
+        biologicalDataDTO.setSampleType(sample.getBiologicalData().getSampleType());
+
+        // Asignamos los datos específicos de la muestra al DTO
+        biologicalDataDTO.setData(sample.getBiologicalData().getData());
+
+        // Asignamos el timestamp del BiologicalData al DTO
+        biologicalDataDTO.setTimestamp(sample.getBiologicalData().getTimestamp());
+
+        // Asignamos el BiologicalDataDTO recien creado al SampleDTO
+        sampleDTO.setBiologicalData(biologicalDataDTO);
+
+        return sampleDTO;
+    }
+
+
+
+    private Sample mapToEntity(final SampleDTO sampleDTO, final Sample sample) {
+        // Asignamos el codigo de la muestra desde el DTO a la entidad Sample
+        sample.setSampleCode(sampleDTO.getSampleCode());
+
+        // Si el experimentId esta presente en el DTO, buscamos el Experiment relacionado
+        final Experiment experiment = sampleDTO.getExperimentId() == null
+                // Si no hay experimentId en el DTO, establecemos experiment como null
+                ? null
+                // Si el experimentId esta presente, buscamos el Experiment en la base de datos
+                : experimentRepository.findById(sampleDTO.getExperimentId())
+                .orElseThrow(() -> new NotFoundException("Experiment no encontrado"));
+        // Asignamos el Experiment encontrado [o null] a la entidad Sample
+        sample.setExperiment(experiment);
+
+        // Si los datos biologicos estan presentes en el DTO, los mapeamos
+        final BiologicalData biologicalData = sampleDTO.getBiologicalData() == null
+                // Si no hay BiologicalData en el DTO, establecemos biologicalData como null
+                ? null
+                // Si hay datos biologicos, los buscamos en la base de datos por su ID
+                : biologicalDataRepository.findById(sampleDTO.getBiologicalData().getId())
+                .orElseThrow(() -> new NotFoundException("Datos biológicos no encontrados"));
+        // Asignamos el BiologicalData encontrado [o null] a la entidad Sample
+        sample.setBiologicalData(biologicalData);
+
+        return sample;
+    }
+
+
+
 }
