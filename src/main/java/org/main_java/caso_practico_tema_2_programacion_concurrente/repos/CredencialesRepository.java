1bfc896c788a95c03f874df8fc451e247fca4f6b
marcos <marcgabe@myuax.com>
2024-10-06 21:01:31 +0200

    Create CredencialesRepository.java
    Repositorio para las credenciales.

---
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/repos/CredencialesRepository.java
@@ -0,0 +1,12 @@
+package org.main_java.caso_practico_tema_2_programacion_concurrente.repos;
+
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Credenciales;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CredencialesRepository extends JpaRepository<Credenciales, Long> {
+
+    // Encuentra credenciales por el nombre de usuario
+    Optional<Credenciales> findByUsername(String username);
+}
