2f3bac46ebd7a07ad207c51999cbe4a64a17e22f
marcos <marcgabe@myuax.com>
2024-10-06 18:55:50 +0200

    Update LabService.java
    Planteamineto basico de lo necesario para dicha clase

--- src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/service/LabService.java
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/service/LabService.java
@@ -1,4 +1,27 @@
 package org.main_java.caso_practico_tema_2_programacion_concurrente.service;
 
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Experiment;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Lab;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.model.LabDTO;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.repos.ExperimentRepository;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.repos.LabRepository;
+import org.main_java.caso_practico_tema_2_programacion_concurrente.util.NotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
 public class LabService {
+
+    private final LabRepository labRepository;
+    private final ExperimentRepository experimentRepository;
+
+    public LabService(LabRepository labRepository, ExperimentRepository experimentRepository) {
+        this.labRepository = labRepository;
+        this.experimentRepository = experimentRepository;
+    }
+    
+
 }
