efae2966b31a01a6896219911669fca2ad85882c
marcos <marcgabe@myuax.com>
2024-10-06 21:01:57 +0200

    Create UsuarioRepository.java
    Repositorio para Usuario.

---
+++ src/main/java/org/main_java/caso_practico_tema_2_programacion_concurrente/repos/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package org.main_java.caso_practico_tema_2_programacion_concurrente.repos;
+
+import org.main_java.caso_practico_tema_2_programacion_concurrente.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    // Encuentra un usuario por su nombre de usuario
+    Optional<Usuario> findByNombre(String nombre);
+
+    // Encuentra un usuario por su correo
+    Optional<Usuario> findByCorreo(String correo);
+
+    // Encuentra un usuario por su id de credenciales
+    Optional<Usuario> findByUsuario_Id(Long credencialesId);
+}
